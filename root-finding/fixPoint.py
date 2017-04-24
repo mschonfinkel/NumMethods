1a404a08ce466310f5d4ee51f18966dc6a8d9a2e
Rkanehisa <rodrigokanehisa@gmail.com>
2017-04-24 17:49:12 -0300

    fixPoint

--- root-finding/fixPoint.py
+++ root-finding/fixPoint.py
@@ -1,16 +1,30 @@
-def fixPoint(f, start, a, b, epsilon=1e-6, k=128):
+import matplotlib.pyplot as plt
+import math
+
+def fixPoint(f, start, a, b, epsilon=1e-10, k=256):
+	xs = []
 	s = start
 	err = 1
 	for i in range(k):
 		x = f(s)
 		err = abs(err - x)
+		xs.append(x)
 		if err < epsilon:
 			break
-	return x
+	return x,xs
 
-'''
-f = lambda x: pow(x, 2) - x + 1
-r = fixPoint(f, 0.9, 0, 2)
 
-print(r, f(r))
-'''
+f = lambda x: pow(x,1/3)
+r,l = fixPoint(f, 0.9, 0, 2)
+
+lis = [f(x) for x in range(10)]
+lis2 = [x for x in range(10)]
+
+plt.plot(lis)
+plt.plot(lis2)
+plt.plot(r,f(r),"o")
+
+print(r,f(r))
+
+plt.show()
+
