189eb59a6ccedc5a362fd40e6f46603678fd2e1d
mschonfinkel <marcos.schonfinkel@gmail.com>
2017-04-25 12:29:01 -0300

    Plot Fix Point graph

--- root-finding/fixPoint.py
+++ root-finding/fixPoint.py
@@ -1,30 +1,46 @@
 import matplotlib.pyplot as plt
-import math
+import numpy as np
 
-def fixPoint(f, start, a, b, epsilon=1e-10, k=256):
+def fixPoint(f, x, g, epsilon1 = 1e-6, epsilon2 = 1e-6, k = 512):
 	xs = []
-	s = start
-	err = 1
 	for i in range(k):
-		x = f(s)
-		err = abs(err - x)
-		xs.append(x)
-		if err < epsilon:
-			break
-	return x,xs
+		if abs(f(x)) < epsilon1:
+			return x, xs
+		x1 = g(x)
+		if abs(f(x1) < epsilon1):
+			return x1, xs
+		if abs(x1 - x) < epsilon2:
+			return x1, xs
+		x = x1
+		xs.append((x, g(x)))
+	return x1, xs
 
+#----------------
+x = np.linspace(-20, 20, 400)
 
-f = lambda x: pow(x,1/3)
-r,l = fixPoint(f, 0.9, 0, 2)
+#----------------
+f = lambda x: pow(x, 3) - x + 1
+g = lambda x: pow(1-x,1/3.0)
 
-lis = [f(x) for x in range(10)]
-lis2 = [x for x in range(10)]
+r, xs = fixPoint(f, 0.5, g)
+print(r, f(r))
 
-plt.plot(lis)
-plt.plot(lis2)
-plt.plot(r,f(r),"o")
+f_np = lambda x: x**3 - x + 1
+y_f = x**3 - x + 1
 
-print(r,f(r))
+#---------------
+fig, ax = plt.subplots()
+
+ax.plot(x, y_f)
+ax.plot(x, x)
+
+ax.set_aspect('equal')
+ax.grid(True, which='both')
+ax.axhline(y=0, color='k')
+ax.axvline(x=0, color='k')
+ax.set_ylim([-15,15])
+ax.set_xlim([-15, 15])
+
+ax.plot(r, g(r), "o")
 
 plt.show()
-
